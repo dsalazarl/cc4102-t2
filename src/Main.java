bc989b757ff3c6294ce8fdca957cf7c159b1db70
Diego Salazar L <dsalazarl@gmail.com>
2018-11-29 18:36:54 -0300

    Ajustes menores y re-documentación de código

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 import data.Graph;
-import data.Pair;
-
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args)
     {
-
         // grafo
         Graph grafoTest = new Graph(20);
 
-        // inicializar
+        // inicialización
         for (int i = 0; i < 19; i++)
         {
             grafoTest.addEdge(i, i + 1, 1.0 / i);
         }
 
-        grafoTest.graphOut();
+        grafoTest.printGraph();
     }
 }
--- src/data/Graph.java
+++ src/data/Graph.java
@@ -2,65 +2,80 @@ package data;
 
 import java.util.ArrayList;
 
-// Clase del grafo, aqui se representara un grafo.
-// La clase dijkstra usara esta representacion
-
-/* graph["i"] devolvera un arraylist "a" que correspondera
-* a una lista de pares de los cuales se puede llegar desde el nodo
-* "i" hasta el nodo a[m].first con peso a[m].second para cualquier
-*  0 <= m < graph["i"].size
-*/
+/**
+ * Clase para representar grafos.
+ *
+ * graph["i"] devolverá un arraylist "a" que corresponderá a una lista de pares
+ * de los cuales se puede llegar desde el nodo "i" hasta el nodo a[m].first,
+ * con peso a[m].second para cualquier 0 <= m < graph["i"].size
+ *
+ * La clase dijkstra usará esta representacion.
+ */
 public class Graph
 {
-    // representacion del grafo
     private ArrayList< ArrayList< Pair<Integer, Double> > > graph;
 
-    // Empty arraylist
+    /**
+     * Constructor de la clase. Solamente inicializa el grafo con una lista vacía.
+     */
     public Graph()
     {
-        this.graph = new ArrayList< ArrayList< Pair<Integer, Double> >>();
+        this.graph = new ArrayList<>();
     }
 
-    // Con numero de nodos descrito
+    /**
+     * Constructor de la clase. Inicializa la estructura dejando listas aristas vacías por cada nodo
+     * @param numNodes cantidad de nodos que tendrá el grafo. Por simplicidad,
+     *                 las ids de los nodos estarán numeradas de 1 hasta numNodes.
+     */
     public Graph(int numNodes)
     {
-        this.graph = new ArrayList< ArrayList< Pair<Integer, Double> >>(numNodes);
+        this();
         for (int i = 0; i < numNodes; i++)
         {
-            ArrayList<Pair<Integer, Double>> inner = new ArrayList<Pair<Integer,Double>>();
+            ArrayList<Pair<Integer, Double>> inner = new ArrayList<>();
             this.graph.add(i, inner);
         }
     }
 
-    // Dado un nodo "desde" y otro de "llegada" y un peso "weight" se añade
-    // una arista desde "desde" hasta "llegada" con peso "weight"
-    // a nuestro grafo
+    /**
+     * Dado un nodo "desde" y otro de "llegada" y un peso "weight" se añade
+     * una arista desde "desde" hasta "llegada" con peso "weight" a nuestro grafo.
+     * @param idNodeFrom nodo de origen.
+     * @param idNodeTo nodo de llegada.
+     * @param weight peso/costo de la arista que va desde el nodo de origen hacia el nodo de llegada.
+     */
     public void addEdge(int idNodeFrom, int idNodeTo, double weight)
     {
         Pair<Integer, Double> edge = new Pair(idNodeTo, weight);
         this.graph.get(idNodeFrom).add(edge);
     }
 
-    // Imprime una descripcion de nuestro grafo a la salida estandar
-    public void graphOut()
+    /**
+     * Imprime una descripcion del grafo a la salida estandar.
+     */
+    public void printGraph()
     {
         System.out.println("***Lista de aristas del grafo***");
         for (int i = 0; i < this.graph.size(); i++)
         {
-
-            System.out.println("Para el nodo: " +i);
+            System.out.println("Para el nodo: " + i);
             ArrayList<Pair<Integer, Double>> nodoI = this.graph.get(i);
 
             for (int j = 0; j < nodoI.size(); j++)
             {
-
-                System.out.print("   Hacia el nodo: "+nodoI.get(j).getFirst());
-                System.out.println(" Con peso: "+nodoI.get(j).getSecond());
+                System.out.print("   Hacia el nodo: " + nodoI.get(j).getFirst());
+                System.out.println(" Con peso: " + nodoI.get(j).getSecond());
             }
         }
     }
 
-    // DEVUELVE LA LISTA DE ARISTAS (pairs) A LAS QUE EL NODO "node" puede llegar
+    /**
+     * Obtener lista de aristas que salen inmediatamente a partir de un nodo.
+     * @param node nodo de origen a partir del cual se desea obtener las aristas que salen de él.
+     * @return lista de aristas que salen de un nodo, expresada como una lista de pares con el
+     *         nodo de llegada y el peso/costo asociado.
+     */
     public ArrayList<Pair<Integer, Double>> getEdges(int node)
     {
         return this.graph.get(node);
